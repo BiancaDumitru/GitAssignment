935c32e8005810864c41f7ec8c84d565e8981bfc
Bell <Bell@Bell-PC>
2020-03-22 15:19:39 +0200

    First Commit

---
+++ GithubAssignment/src/ro/ase/csie/cts/laborator/assignment2/TestGit.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.laborator.assignment2;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git ! Denumirea proiectului de licenta este: "
+				+ "Aplicatie mobila pentru facilitarea unui stil de viata echilibrat.");
+	}
+
+}
